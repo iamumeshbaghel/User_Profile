65939c844085b1b590c3f37616c705016be095b2
UMESH BAGHEL <63281733+iamumeshbaghel@users.noreply.github.com>
2021-05-15 22:01:09 +0530

    Update MainActivity.java

--- app/src/main/java/baghelstudio/userprofile/MainActivity.java
+++ app/src/main/java/baghelstudio/userprofile/MainActivity.java
@@ -11,4 +11,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void on_contact(View view) {
+        intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.setType("message/rfc822");
+        String[] send_to = {"erumeshbaghel@gmail.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL, send_to);
+        //intent.putExtra(Intent.EXTRA_BCC, address);
+        //intent.putExtra(Intent.EXTRA_CC, address);
+
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Learn MIUI Theme Development App Feedback!");
+        intent.putExtra(Intent.EXTRA_TEXT, "Dear Developer,");
+        chooser = Intent.createChooser(intent, "Select Email App...");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
+    }
 }
